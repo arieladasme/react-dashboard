54b98755941b541e079b5e087f6dbf003d5d42a5
ariel <ariel.wm1@gmail.com>
2023-02-05 19:29:20 -0300

    adding configs

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+// craco.config.js
+/* module.exports = {
+  style: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
+} */
+
+export const style = {
+  postcss: {
+    plugins: [require('tailwindcss'), require('autoprefixer')],
+  },
+}
