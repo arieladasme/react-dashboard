e1c9b306bc319383312318f416b4e42d2a2a4efc
ariel <ariel.wm1@gmail.com>
2023-02-07 15:01:15 -0300

    fix icon size

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -11,9 +11,8 @@ export const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       <span
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
         style={{ background: dotColor }}
-      >
-        {icon}
-      </span>
+      />
+      {icon}
     </button>
   </TooltipComponent>
 )
