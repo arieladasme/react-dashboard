f5b55530eb81ec5dca5c754d4ff1a825145c6197
ariel <ariel.wm1@gmail.com>
2023-02-07 13:30:00 -0300

    Navbar

---
+++ src/components/NavButton.jsx
@@ -0,0 +1,19 @@
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+
+export const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position="BottomCenter">
+    <button
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+      type="button"
+      onClick={customFunc}
+      style={{ color }}
+    >
+      <span
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+        style={{ background: dotColor }}
+      >
+        {icon}
+      </span>
+    </button>
+  </TooltipComponent>
+)
