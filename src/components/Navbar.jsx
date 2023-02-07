3833c0f57e940813ecc6221d3619d572b876a33d
ariel <ariel.wm1@gmail.com>
2023-02-07 14:40:18 -0300

    adding screenSize

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import { AiOutlineMenu } from 'react-icons/ai'
 import { BsChatLeft } from 'react-icons/bs'
@@ -11,7 +12,28 @@ import { Cart, Chat, Notification, UserProfile } from './'
 import avatar from '../data/avatar.jpg'
 
 export const Navbar = () => {
-  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useStateContext()
+  const {
+    activeMenu,
+    setActiveMenu,
+    isClicked,
+    setIsClicked,
+    handleClick,
+    screenSize,
+    setScreenSize,
+  } = useStateContext()
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+    handleResize()
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  useEffect(() => {
+    screenSize <= 900 ? setActiveMenu(false) : setActiveMenu(true)
+  }, [screenSize])
+
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       <NavButton
