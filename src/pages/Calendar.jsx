6e8028ef223c424757ed728dea95552913626098
ariel <ariel.wm1@gmail.com>
2023-02-08 17:04:31 -0300

    adding CalendarApp

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,3 +1,28 @@
+import {
+  Agenda,
+  Day,
+  Month,
+  ScheduleComponent,
+  Week,
+  WorkWeek,
+  Resize,
+  DragAndDrop,
+  Inject,
+} from '@syncfusion/ej2-react-schedule'
+import { Header } from '../components'
+import { scheduleData } from '../data/dummy'
+
 export const Calendar = () => {
-  return <div>Calendar</div>
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2021, 0, 10)}
+      >
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
+    </div>
+  )
 }
