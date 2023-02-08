64c4360d0c2e1b271fb0ea3145ade1b0a915eff3
ariel <ariel.wm1@gmail.com>
2023-02-08 14:18:14 -0300

    adding grid structure

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,18 @@
+import { GridComponent, ColumnDirective, ColumnsDirective } from '@syncfusion/ej2-react-grids'
 import { Header } from '../components'
+import { ordersGrid } from '../data/dummy'
 
 export const Orders = () => {
   return (
     <div className=" m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
+      <GridComponent>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </GridComponent>
     </div>
   )
 }
