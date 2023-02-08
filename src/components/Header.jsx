dfc1611ddda175ba3219572e4e119e56a021039a
ariel <ariel.wm1@gmail.com>
2023-02-08 14:14:18 -0300

    adding header in Orders

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,8 @@
-export const Header = () => {
-  return <div>Header</div>
+export const Header = ({ category, title }) => {
+  return (
+    <div className="mb-10">
+      <p className="text-gray-400">{category}</p>
+      <p className="text-3x1 font-extrabold tracking-tight text-slate-900">{title}</p>
+    </div>
+  )
 }
