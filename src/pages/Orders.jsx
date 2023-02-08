ae7dc15476d0f5ae03a47db7635a4401a960ae89
ariel <ariel.wm1@gmail.com>
2023-02-08 15:00:46 -0300

    adding data in grid

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,12 +1,12 @@
 import { GridComponent, ColumnDirective, ColumnsDirective } from '@syncfusion/ej2-react-grids'
 import { Header } from '../components'
-import { ordersGrid } from '../data/dummy'
+import { ordersData, ordersGrid } from '../data/dummy'
 
 export const Orders = () => {
   return (
     <div className=" m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
-      <GridComponent>
+      <GridComponent id="gridcomp" dataSource={ordersData}>
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
