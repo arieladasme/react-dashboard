84e411d916a7b60acaa1b56458bdd98e5d744a02
ariel <ariel.wm1@gmail.com>
2023-02-09 13:44:29 -0300

    area graph & fix sparkLine

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,3 +1,38 @@
+import {
+  ChartComponent,
+  DateTime,
+  Inject,
+  Legend,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  SplineAreaSeries,
+} from '@syncfusion/ej2-react-charts'
+import { Header } from '../../components'
+import { useStateContext } from '../../contexts/ContextProvider'
+import { areaCustomSeries, areaPrimaryXAxis, areaPrimaryYAxis } from '../../data/dummy'
+
 export const Area = () => {
-  return <div>Area</div>
+  const { currentMode } = useStateContext()
+
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Area" title="Inflation Rate in Percentage" />
+      <ChartComponent
+        id="area-chart"
+        height="420px"
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === 'Dark' ? '#33373E' : '#FFF'}
+      >
+        <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  )
 }
