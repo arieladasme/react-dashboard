ab535fd9e988d5830895abdbc18ad088632f0431
ariel <ariel.wm1@gmail.com>
2023-02-09 13:27:44 -0300

    line chart

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,3 +1,35 @@
+import {
+  ChartComponent,
+  DateTime,
+  Inject,
+  Legend,
+  LineSeries,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Tooltip,
+} from '@syncfusion/ej2-react-charts'
+import { useStateContext } from '../../contexts/ContextProvider'
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy'
+
 export const LineChart = () => {
-  return <div>LineChart</div>
+  const { currentMode } = useStateContext()
+
+  return (
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#FFF'}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  )
 }
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,3 +1,12 @@
+import { Header, LineChart } from '../../components'
+
 export const Line = () => {
-  return <div>Line</div>
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Chart" title="Inflation Rate" />
+      <div className="w-full">
+        <LineChart />
+      </div>
+    </div>
+  )
 }
