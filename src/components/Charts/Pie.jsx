ff9b6f31b5d2233962423f68e97c368efbcd347d
ariel <ariel.wm1@gmail.com>
2023-02-13 11:35:56 -0300

    pie chart

--- src/components/Charts/Pie.jsx
+++ src/components/Charts/Pie.jsx
@@ -1,3 +1,50 @@
+import {
+  AccumulationChartComponent,
+  AccumulationDataLabel,
+  AccumulationLegend,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  AccumulationTooltip,
+  Inject,
+  PieSeries,
+} from '@syncfusion/ej2-react-charts'
+import { pieChartData } from '../../data/dummy'
+import { Header } from '../Header'
+
 export const Pie = () => {
-  return <div>Pie</div>
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Chart" title="Inflation Rate in Percentage" />
+      <AccumulationChartComponent
+        id="charts"
+        legendSettings={{
+          visible: true,
+        }}
+        enableSmartLabels={true}
+        enableAnimation={true}
+        tooltip={{ enable: true }}
+      >
+        <Inject
+          services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]}
+        />
+        <AccumulationSeriesCollectionDirective>
+          <AccumulationSeriesDirective
+            dataSource={pieChartData}
+            xName="x"
+            yName="y"
+            type="Pie"
+            explode={true}
+            explodeOffset="5%"
+            explodeIndex={0}
+            dataLabel={{
+              visible: true,
+              position: 'Outside',
+              name: 'x',
+            }}
+            radius="r"
+          ></AccumulationSeriesDirective>
+        </AccumulationSeriesCollectionDirective>
+      </AccumulationChartComponent>{' '}
+    </div>
+  )
 }
